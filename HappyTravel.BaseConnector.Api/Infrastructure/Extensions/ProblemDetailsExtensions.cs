a339831c51ee5ad23500cf8fd132f647098d7ce4
Konstantin Yefremov <konstyefremov@gmail.com>
2022-07-14 14:21:30 +0400

    Add summary to public methods

--- HappyTravel.BaseConnector.Api/Infrastructure/Extensions/ProblemDetailsExtensions.cs
+++ HappyTravel.BaseConnector.Api/Infrastructure/Extensions/ProblemDetailsExtensions.cs
@@ -6,6 +6,12 @@ namespace HappyTravel.BaseConnector.Api.Infrastructure.Extensions;
 
 public static class ProblemDetailsExtensions
 {
+    /// <summary>
+    /// Adds booking failure code to extensions of ProblemDetails
+    /// </summary>
+    /// <param name="problemDetails">ProblemDetails to add to booking failure code</param>
+    /// <param name="code">Booking failure code</param>
+    /// <returns>ProblemDetails with BookingFailureCode in extensions</returns>
     public static ProblemDetails AddBookingFailureCode(this ProblemDetails problemDetails, BookingFailureCodes code)
     {
         problemDetails.Extensions.AddBookingFailureCode(code);
@@ -18,7 +24,7 @@ public static class ProblemDetailsExtensions
     /// </summary>
     /// <param name="problemDetails">ProblemDetails to add to search failure code</param>
     /// <param name="code">Search failure code</param>
-    /// <returns></returns>
+    /// <returns>ProblemDetails with SearchFailureCode in extensions</returns>
     public static ProblemDetails AddSearchFailureCodes(this ProblemDetails problemDetails, SearchFailureCodes code)
     {
         problemDetails.Extensions.AddSearchFailureCode(code);
